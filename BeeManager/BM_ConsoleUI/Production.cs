6e956485683e24c9393f26293ed35604a85577d0
kristapspriede <kristaps.priede@gmail.com>
2021-10-11 20:51:40 +0300

    Created Production class

---
+++ BeeManager/BM_ConsoleUI/Production.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM_ConsoleUI
+{
+    public class Production
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int ProductId { get; set; }
+        public double Quantity { get; set; }
+        public int UnitsOfMeasurementId { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<UnitsOfMeasurement> Units { get; set; }
+    }
+}
