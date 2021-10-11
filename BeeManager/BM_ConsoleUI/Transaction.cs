31efbf91ff02ca5393b7d9d90f3b95f6d158ac05
Aleksejs <aleksejs.ivanovskis@gmail.com>
2021-10-11 21:48:26 +0300

    Created Transaction class and associated classes

---
+++ BeeManager/BM_ConsoleUI/Transaction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM_ConsoleUI
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int TypeId { get; set; }
+        public string Description  { get; set; }
+        public int ObjectId { get; set; }
+        public double Quantity { get; set; }
+        public int UnitOfMeasurementId { get; set; }
+        public double PricePerUnit { get; set; }
+        public decimal Total { get; private set; }
+
+        public Transaction(DateTime transactionDate, int transactionTypeId, int transactionObjectId, 
+                           string description, double quantity, int unitOfMeasurementId, double pricePerUnit)
+        {
+            Date = transactionDate;
+            TypeId = transactionTypeId;
+            ObjectId = transactionObjectId;
+            Description = description;
+            Quantity = quantity;
+            UnitOfMeasurementId = unitOfMeasurementId;
+            PricePerUnit = pricePerUnit;
+            Total = CalculateTotal();
+
+        }
+
+
+        private decimal CalculateTotal()
+        {
+            return (decimal)(PricePerUnit * Quantity);
+        }
+    }
+}
---
+++ BeeManager/BM_ConsoleUI/TransactionObjectType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BM_ConsoleUI
+{
+    class TransactionObjectType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
