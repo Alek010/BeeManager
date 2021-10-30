3fba82167587ddcad0e57ed62d2b1928ac4a3821
kristapspriede <kristaps.priede@gmail.com>
2021-10-31 00:15:19 +0300

    Added Production list to MVC

---
+++ BeeManager/BeeManagerMVC/Models/ProductionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeeManagerMVC.Models
+{
+    public class ProductionModel
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Product { get; set; }
+        public double Quantity { get; set; }
+        public string Units { get; set; }
+    }
+}
