d9d97dac5e3996caeb281174c055955fb0c13cf9
Aleksejs <aleksejs.ivanovskis@gmail.com>
2021-11-04 19:49:35 +0200

    ProductionSummmuryModel added to MVC

---
+++ BeeManager/BeeManagerMVC/Models/ProductionSummaryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BeeManagerMVC.Models
+{
+    public class ProductionSummaryModel
+    {
+        public int Year { get; set; }
+        public string ProductName { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
